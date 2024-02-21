4254645574df212ee8e13fbc69fce6de4f3aec7c
aliviasutherland <157061925+aliviasutherland@users.noreply.github.com>
2024-02-21 12:48:18 -0500

    Add files via upload

---
+++ project it part 1 tkinter.py
@@ -0,0 +1,88 @@
+
+print ("Would you like to schedule an appointment with housekeeping?")
+valid_response = False
+while not valid_response:
+    response = input("Enter 'yes' or 'no': ")
+    if response.lower() == 'yes' or response.lower() == 'no':
+        valid_response = True
+    else:
+        print("Invalid response. Please enter 'yes' or 'no'.")
+ 
+from tkinter import *
+ 
+
+import calendar
+ 
+def showCal() :
+ 
+   
+    new_gui = Tk()
+     
+    new_gui.config(background = "black")
+ 
+    new_gui.title("HOUSEKEEPING CALENDAR")
+ 
+    new_gui.geometry("600x600")
+ 
+    fetch_year = int(year_field.get())
+ 
+    
+    cal_content = calendar.calendar(fetch_year)
+ 
+    cal_year = Label(new_gui, text = cal_content, font = "Consolas 10 bold")
+ 
+   
+    cal_year.grid(row = 5, column = 1, padx = 20)
+     
+  
+    new_gui.mainloop()
+ 
+     
+
+if __name__ == "__main__" :
+ 
+    
+    gui = Tk()
+     
+    gui.config(background = "black")
+ 
+    gui.title(" HOUSEKEEPING CALENDAR")
+ 
+   
+    gui.geometry("250x140")
+ 
+    cal = Label(gui, text = "CALENDAR", bg = "dark gray",
+                            font = ("times", 28, 'bold'))
+ 
+    year = Label(gui, text = "Enter Year", bg = "light blue")
+     
+    year_field = Entry(gui)
+ 
+  
+    Show = Button(gui, text = "Show Calendar", fg = "Black",
+                              bg = "Red", command = showCal)
+ 
+    Exit = Button(gui, text = "Exit", fg = "Black", bg = "Red", command = exit)
+     
+   
+    cal.grid(row = 1, column = 1)
+ 
+    year.grid(row = 2, column = 1)
+ 
+    year_field.grid(row = 3, column = 1)
+ 
+    Show.grid(row = 4, column = 1)
+ 
+    Exit.grid(row = 6, column = 1)
+     
+   
+    gui.mainloop()
+
+    print ("Now choose a date that is suitable for you")
+    valid_response = False
+while not valid_response:
+    response = input("Enter 'yes' or 'no': ")
+    if response.lower() == 'yes' or response.lower() == 'no':
+        valid_response = True
+    else:
+        print("Invalid response. Please enter 'yes' or 'no'.")
